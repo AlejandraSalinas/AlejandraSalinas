18030a330c0a604d893768056b35a0b638b8f669
ErinPaola <erinpaolalopeztriana2018@gmail.com>
2023-06-28 11:09:12 -0500

    ajustes

--- Views/Main/partials/header.php
+++ Views/Main/partials/header.php
@@ -12,6 +12,7 @@
 
     <title>Registro y Control</title>
 
+    <!-- Custom fonts for this template-->
     <link href="../../Public/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -19,6 +20,8 @@
     <link href="../../Public/css/sb-admin-2.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../public/css/estilos.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <!-- <link rel="stylesheet" href="../../../Public/js/demo/restripciones.js"> -->
 </head>
 
 <body id="page-top">
