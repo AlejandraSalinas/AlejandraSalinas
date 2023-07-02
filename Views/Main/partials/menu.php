e87c6dc32e6e803ca3c6cca144bc28f3e9341fe9
ErinPaola <erinpaolalopeztriana2018@gmail.com>
2023-07-01 19:20:12 -0500

    AJUSTES

--- Views/Main/partials/menu.php
+++ Views/Main/partials/menu.php
@@ -1,6 +1,6 @@
 <?php require_once dirname(__FILE__) . '../../../../Config/config.php'; ?>
 
-<!-- Nav Item - Pages Collapse Menu -->
+
 <li class="nav-item">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUsuarios" aria-expanded="true" aria-controls="collapseUsuarios">
         <i class="bi bi-people-fill" style="font-size: 1.4rem;"></i>
@@ -14,22 +14,8 @@
         </div>
     </div>
 </li>
-
-<!-- Nav Item - Utilities Collapse Menu -->
-<li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseDispositivos" aria-expanded="true" aria-controls="collapseDispositivos">
-        <i class="bi bi-pc-display" style="font-size: 1.4rem;"></i>
-        <span>Dispositivos</span>
-    </a>
-    <div id="collapseDispositivos" class="collapse" aria-labelledby="headingDispositivos" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Opciones:</h6>
-            <a class="collapse-item" href="<?php constant('URL') ?>../../Views/dispositivos/index.php">Ver</a>
-            <a class="collapse-item" href="<?php constant('URL') ?>../../Views/dispositivos/crear.php">Crear</a>
-        </div>
-    </div>
-</li>
 <hr class="sidebar-divider">
+
 <li class="nav-item">
     <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseinformacionVigilante" aria-expanded="true" aria-controls="collapseinformacionVigilante">
         <i class="bi bi-person-square" style="font-size: 1.4rem;"></i>
@@ -42,10 +28,43 @@
             <a class="collapse-item" href="<?php constant('URL') ?>../../Views/vigilante/index.php">Ver</a>
         </div>
     </div>
+
 </li>
 <hr class="sidebar-divider">
+
 <li class="nav-item">
-    <a class="nav-link" href="<?= constant('URL') ?>./Views/tipoIdentificacion/index.php">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseDispositivos" aria-expanded="true" aria-controls="collapseDispositivos">
+        <i class="bi bi-pc-display" style="font-size: 1.4rem;"></i>
+        <span>Dispositivos</span>
+    </a>
+    <div id="collapseDispositivos" class="collapse" aria-labelledby="headingDispositivos" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Opciones:</h6>
+            <a class="collapse-item" href="<?php constant('URL') ?>../../Views/dispositivos/menuDispositivo.php">Ingreso/Egreso</a>
+            <a class="collapse-item" href="<?php constant('URL') ?>../../Views/dispositivos/index.php">Ver</a>
+            <a class="collapse-item" href="<?php constant('URL') ?>../../Views/dispositivos/crear.php">Crear</a>
+        </div>
+    </div>
+</li>
+<hr class="sidebar-divider">
+
+<li class="nav-item">
+<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseDispositivos" aria-expanded="true" aria-controls="collapseDispositivos">
+    <i class="bi bi-pc-display" style="font-size: 1.4rem;"></i>
+    <span>Accesorios</span>
+</a>
+<div id="collapseDispositivos" class="collapse" aria-labelledby="headingDispositivos" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+        <h6 class="collapse-header">Opciones:</h6>
+        <a class="collapse-item" href="<?php constant('URL') ?>../../Views/accesorios/index.php">Ver</a>
+        <a class="collapse-item" href="<?php constant('URL') ?>../../Views/accesorios/crear.php">Crear</a>
+    </div>
+</div>
+</li>
+<hr class="sidebar-divider">
+
+<li class="nav-item">
+    <a class="nav-link" href="<?= constant('URL') ?>./Views/configMarca/index.php">
         <i class="bi bi-gear-fill" style="font-size: 1.4rem;"></i>
         <span>Configuración</span>
     </a>
